8ccc4c77781e5087062ebef2ce22e6d9316d0895
Andrii Zadaianchuk <azadaianchuk@urft.is.localnet>
2017-11-09 11:30:10 +0100

    delete outdated code

--- run_ProbRPROP_mnist.py
+++ run_ProbRPROP_mnist.py
@@ -67,19 +67,11 @@ IMAGE_SIZE = 784
 
 
 def train_model(model, batch_gen, num_train_steps,eval_every):
-    # saver = tf.train.Saver() #to_save_variables
-
-    # initial_step = 0
-    # util.make_dir('checkpoints')
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        # ckpt = tf.train.get_checkpoint_state(os.path.dirname('checkpoints/checkpoint'))
-        # if that checkpoint exists, restore from checkpoint
-        # if ckpt and ckpt.model_checkpoint_path:
-        #     saver.restore(sess, ckpt.model_checkpoint_path)
         total_loss = 0.0 # we use this to calculate late average loss in the last SKIP_STEP steps
 
-        dir_to_save="../summary/"+model.name+"/"
+        dir_to_save="../summary_test/"+model.name+"/"
         unique_str=model.opt_name+", lr=" + str(model.lr) + ", batch_size=" + str(model.batch_size)
         writer = tf.summary.FileWriter(dir_to_save+unique_str, sess.graph)
 
@@ -95,9 +87,6 @@ def train_model(model, batch_gen, num_train_steps,eval_every):
             writer.add_summary(hist_summary, global_step=index)
             if (index + 1) % eval_every == 0:
                 print('Average loss at step {}: {:5.3f}'.format(index, total_loss / eval_every))
-                # total_loss = 0.0
-                # loss = []
-                # accuracy = []
                 # when testing the classifier
                 with tf.name_scope("streaming"):
                     # clear counters for a fresh evaluation
@@ -105,19 +94,6 @@ def train_model(model, batch_gen, num_train_steps,eval_every):
                 for _ in range(NUM_VAL_STEPS+1):
                     batch = mnist.train.next_batch(BATCH_SIZE)
                     stream,slow_summary_train= sess.run([model.stream_fetches,model.slow_train_summary], {model.x: batch[0], model.y_: batch[1]})
-                    total_loss=stream["loss"]
-                #   loss.append(l)
-                #   accuracy.append(a)
-                # loss = np.mean(loss) #try cast
-                # accuracy = np.mean(accuracy)
-                # print ("train: loss", loss, "acc", accuracy)
-
-                # sum_accuracy = tf.Summary()
-                # sum_loss = tf.Summary()
-                # sum_accuracy.value.add(tag="summaries/evaluation/train_accuracy", simple_value=sum_accuracy)
-                # sum_loss.value.add(tag="summaries/evaluation/train_loss" , simple_value=sum_loss)
-                # writer.add_summary(sum_loss, global_step=index)
-                # writer.add_summary(sum_accuracy, global_step=index)
                 total_loss=stream["loss"]
                 writer.add_summary(slow_summary_train, global_step=index)
 
@@ -129,36 +105,6 @@ def train_model(model, batch_gen, num_train_steps,eval_every):
                     stream,slow_summary_test= sess.run([model.stream_fetches,model.slow_test_summary], {model.x: batch[0], model.y_: batch[1]})
                     total_loss=stream["loss"]
                 writer.add_summary(slow_summary_test, global_step=index)
-                # loss = []
-                # accuracy = []
-                # for _ in range(NUM_VAL_STEPS):
-                #   batch = mnist.test.next_batch(BATCH_SIZE)
-                #   l, a = sess.run([model.loss, model.accuracy], {model.x: batch[0], model.y_: batch[1]})
-                #   loss.append(l)
-                #   accuracy.append(a)
-                # loss = np.mean(loss)
-                # accuracy = np.mean(accuracy)
-                # print ("test: loss", loss, "acc", accuracy)
-                #
-                # sum_accuracy = tf.Summary()
-                # sum_loss = tf.Summary()
-                # sum_accuracy.value.add(tag="summaries/evaluation/test_accuracy", value=sum_accuracy)
-                # sum_loss.value.add(tag="summaries/evaluation/test_loss" , value=sum_loss)
-                # writer.add_summary(sum_loss, global_step=index)
-                # writer.add_summary(sum_accuracy, global_step=index)
-
-                # # saver.save(sess, 'checkpoints/LR/RPROP', index)
-                # feed_dict_train = {model.x: mnist.train.images, model.y_: mnist.train.labels} #big batch of data
-                # feed_dict_test = {model.x: mnist.test.images, model.y_: mnist.test.labels}
-                #
-                # #calculate our summaries
-                # loss_train,slow_summary_train = sess.run([model.loss, model.slow_train_summary],
-                #                                   feed_dict=feed_dict_train)
-                # accuracy_test,slow_summary_test = sess.run([model.accuracy, model.slow_test_summary],
-                #                                   feed_dict=feed_dict_test)
-                #
-                # writer.add_summary(slow_summary_test, global_step=index)
-                # writer.add_summary(slow_summary_train, global_step=index)
 
 
 
