3851dc5caf35c203aee0d4710812598d0ad183e3
Andrii Zadaianchuk <azadaianchuk@urft.is.localnet>
2017-11-13 13:37:28 +0100

    modify ProbRPROP in the same way as RPROP

--- rprop_optimizer.py
+++ rprop_optimizer.py
@@ -59,8 +59,6 @@ class ProbRPROPOptimizer(tf.train.GradientDescentOptimizer):
         # power of mu for bias-corrected first and second moment estimate
         mu_power = tf.get_variable("mu_power", shape=(), dtype=tf.float32, trainable=False, initializer=tf.constant_initializer(1.0))
 
-        # save old mean and variance of grads
-        old_ms = ms
 
         grads = tf.gradients(loss, var_list)
         grads_squared = [tf.square(g) for g in grads]
@@ -94,83 +92,81 @@ class ProbRPROPOptimizer(tf.train.GradientDescentOptimizer):
             # find sign of product of gradients
             grads_sign = [tf.sign(grad) for grad in grads]
             old_grads_sign = [tf.sign(old_grad) for old_grad in old_grads]
-            mult_sign_masks = [tf.multiply(grad_sign,old_grad_sign)
+            prods = [tf.multiply(grad_sign,old_grad_sign)
                                for grad_sign,old_grad_sign
                                in zip(grads_sign,old_grads_sign)]
-            # check the product of signs
-            conds_equal = [tf.equal(mult_sign_mask,
-                                    tf.zeros_like(mult_sign_mask)) for mult_sign_mask in mult_sign_masks]
-            conds_less = [tf.less(mult_sign_mask,
-                                  tf.zeros_like(mult_sign_mask))
-                          for mult_sign_mask in mult_sign_masks]
-            conds_greater = [tf.greater(mult_sign_mask,tf.zeros_like(mult_sign_mask))
-                             for mult_sign_mask in mult_sign_masks]
+            with tf.control_dependencies(prods+probs):
+                # check the product of signs
+                conds_equal = [tf.equal(prod,tf.zeros_like(prod)) for prod in prods]
+                conds_less = [tf.less(prod,tf.zeros_like(prod)) for prod in prods]
+                conds_greater = [tf.greater(prod,tf.zeros_like(prod)) for prod in prods]
 
-            # count the number of sign changes, the same signs and zero products
-            # for every variable tensor
-            # used for tracking of opt performance
-            count_less=[tf.reduce_sum(tf.cast(cond_less,tf.int64))
-                        for cond_less in conds_less]
-            count_greater=[tf.reduce_sum(tf.cast(cond_greater,tf.int64))
-                           for cond_greater in conds_greater]
-            count_equal=[tf.reduce_sum(tf.cast(cond_equal,tf.int64))
-                         for cond_equal in conds_equal ]
-            # summary switch
-            switch=tf.add_n(count_less)
-            no_switch=tf.add_n(count_greater)
-            zero_prod=tf.add_n(count_equal)
-            n_of_parameters=switch+ no_switch + zero_prod
-            sign_changes={"switch": switch/n_of_parameters,"no_switch":no_switch/n_of_parameters,
-                          "zero_prod":zero_prod/n_of_parameters,"switch_prob": switch_prob/n_of_parameters}
+                # count the number of sign changes, the same signs and zero products
+                # for every variable tensor
+                # used for tracking of opt performance
+                count_less=[tf.reduce_sum(tf.cast(cond_less,tf.int64))
+                            for cond_less in conds_less]
+                count_greater=[tf.reduce_sum(tf.cast(cond_greater,tf.int64))
+                               for cond_greater in conds_greater]
+                count_equal=[tf.reduce_sum(tf.cast(cond_equal,tf.int64))
+                             for cond_equal in conds_equal ]
+                # summary switch
+                switch=tf.add_n(count_less)
+                no_switch=tf.add_n(count_greater)
+                zero_prod=tf.add_n(count_equal)
+                n_of_parameters=switch+ no_switch + zero_prod
+                sign_changes={"switch": switch/n_of_parameters,"no_switch":no_switch/n_of_parameters,
+                              "zero_prod":zero_prod/n_of_parameters,"switch_prob": switch_prob/n_of_parameters}
 
-            zeros=[tf.zeros_like(old_delta) for old_delta in old_deltas]
+                zeros=[tf.zeros_like(old_delta) for old_delta in old_deltas]
+                # calculate the all possible delta updates
+                deltas_less=[tf.maximum(d*eta_minus,
+                                        tf.cast(tf.ones_like(d),tf.float32)*delta_min) for d in old_deltas]
+                deltas_greater=[tf.minimum(d*eta_plus,tf.ones_like(d)*delta_max) for d in old_deltas]
 
+                # select delta updates using cond tensors
+                deltas=old_deltas
+                deltas=[tf.where(cond_less, delta_less, delta) for (cond_less, delta_less,
+                                                                    delta)
+                            in zip(conds_less,deltas_less, deltas)]
+                deltas=[tf.where(cond_greater, delta_greater, delta)
+                        for (cond_greater, delta_greater, delta) in zip(conds_greater,
+                                                                        deltas_greater, deltas)]
 
-            # calculate the all possible delta updates
-            deltas_equal=old_deltas
-            deltas_less=[tf.maximum(d*eta_minus,
-                                    tf.cast(tf.ones_like(d),tf.float32)*delta_min) for d in old_deltas]
-            deltas_greater=[tf.minimum(d*eta_plus,tf.ones_like(d)*delta_max) for d in old_deltas]
+                # save new the deltas
+                old_deltas_updates = [old_delta.assign(delta)
+                                      for (old_delta, delta) in zip(old_deltas,deltas)]
 
-            # select delta updates using cond tensors
-            deltas=zeros
-            deltas=[tf.where(cond_equal, delta_equal, delta) for (cond_equal,
-                                                                  delta_equal,delta)
-                        in zip(conds_equal,deltas_equal, deltas)]
-            deltas=[tf.where(cond_less, delta_less, delta) for (cond_less, delta_less,
-                                                                delta)
-                        in zip(conds_less,deltas_less, deltas)]
-            deltas=[tf.where(cond_greater, delta_greater, delta)
-                    for (cond_greater, delta_greater, delta) in zip(conds_greater,
-                                                                    deltas_greater, deltas)]
+                # w update directions
+                # select no update in case of negative product
+                # or dir_geq update in other cases
+                with tf.control_dependencies(old_deltas_updates):
+                    if MAKE_NEG_STEP:
+                        dirs = [-delta*grad_sign
+                                  for  (delta,grad_sign) in zip(deltas,grads_sign)]
+                    else:
+                        dirs=zeros
+                        dirs_geq=[-delta*grad_sign
+                                  for  (delta,grad_sign) in zip(deltas,grads_sign)]
+                        dirs=[tf.where(tf.logical_or(cond_equal,cond_greater), dir_geq, d)
+                              for (cond_equal,cond_greater,dir_geq,d)
+                              in zip(conds_equal,conds_greater,dirs_geq, dirs)]
 
-            # save new the deltas
-            old_deltas_updates = [old_delta.assign(delta)
-                                  for (old_delta, delta) in zip(old_deltas,deltas)]
+                    # change grad to zero in case of negative product and save new gradients
+                    grads=[tf.where(cond_less,zero,grad)
+                                for (cond_less,zero,grad) in zip(conds_less,zeros,grads)]
+                    old_grads_updates = [old_grad.assign(g)
+                                         for (old_grad, g) in zip(old_grads, grads)]
 
-            # calculate parameters update
-            dirs_geq=[-delta*grad_sign
-                      for  (delta,grad_sign) in zip(deltas,grads_sign)]
+                    with tf.control_dependencies(dirs):
+                        old_grads_updates = [old_grad.assign(g)
+                                             for (old_grad, g) in zip(old_grads, grads)]
 
-            # select no update in case of negative product
-            # or dir_geq update in other cases
-            dirs=zeros
-            dirs=[tf.where(tf.logical_or(cond_equal,cond_greater), dir_geq, d)
-                  for (cond_equal,cond_greater,dir_geq,d)
-                  in zip(conds_equal,conds_greater,dirs_geq, dirs)]
-
-            # change grad to zero in case of negative product and save new gradients
-            grads=[tf.where(cond_less,zero,grad_sign)
-                        for (cond_less,zero,grad_sign) in zip(conds_less,zeros,grads)]
-            with tf.control_dependencies(probs+dirs):
-                old_grads_updates = [old_grad.assign(g)
-                                     for (old_grad, g) in zip(old_grads, grads)]
-
-            # here learning rate is scaling parameter, by default equal 1
-            with tf.control_dependencies(old_grads_updates+old_deltas_updates):
-                variable_updates = [v.assign_add(self._lr*d) for v, d in zip(var_list, dirs)]
-            for_summaries ={"sign": sign_changes,"prob":probs,"snr":abs_snrs,"delta": deltas,"mu_power": mu_power}
-        return tf.group(*variable_updates),for_summaries
+                    # here learning rate is scaling parameter, by default equal 1
+                    with tf.control_dependencies(old_grads_updates):
+                        variable_updates = [v.assign_add(self._lr*d) for v, d in zip(var_list, dirs)]
+                        for_summaries ={"sign": sign_changes,"prob":probs,"snr":abs_snrs,"delta": deltas,"mu_power": mu_power}
+            return tf.group(*variable_updates),for_summaries
 
 class RPROPOptimizer(tf.train.GradientDescentOptimizer):
 
@@ -288,5 +284,5 @@ class RPROPOptimizer(tf.train.GradientDescentOptimizer):
             # we need to check how quikly we change it during training
             with tf.control_dependencies(old_grads_updates):
                 variable_updates = [v.assign_add(self._lr*d) for v, d in zip(var_list, dirs)]
-        for_summaries = {"sign": sign_changes, "delta": deltas}
+                for_summaries = {"sign": sign_changes, "delta": deltas}
         return tf.group(*variable_updates),for_summaries
